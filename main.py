11c84145b0283c9d71089170fddfdd2d59835e67
jay841224 <j1360fy6416nb@gmail.com>
2019-11-05 12:26:15 +0800

    defined initial partical number and output file

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import initial, wolf
 
 def main():
-    wolf = []
-    wolf.append(initial.ran_partical(10))
-    with open('initial.txt', 'w+') as file:
-        for x in wolf:
-            for y in x._A:
-                file.write('{}\n'.format(y))
+    #初始族群
+    number = 2
+    wolf = initial.ran_partical(number)
+    for x, count in zip(wolf, range(len(wolf))):
+        file = open('partical{}.txt'.format(str(count)), 'w+')
+        for y in x._A:
+            file.write('{}\n'.format(y))
+        file.close()
 
 if __name__ == "__main__":
     main()
