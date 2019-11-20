bf57d90747edd34eab00e6c5b04caf07b7edd9e0
jay841224 <j1360fy6416nb@gmail.com>
2019-11-20 14:21:46 +0800

    add pbest

--- main.py
+++ main.py
@@ -12,13 +12,10 @@ def main():
         file.close()
 
     for x, count in zip(wolfs, range(len(wolfs))):
-        with open('pbest{}.txt'.format(str(count)), 'w') as file:
-            for xp in x._pbest:
-                file.write('{} '.format(xp))
-            file.write('\n')
-            file.write('{}'.format(x._pbestfit))
-
-    
+        file = open('pbest{}.txt'.format(str(count)), 'w+')
+        for y in x._A:
+            file.write('{}\n'.format(y))
+        file.close()
 
 if __name__ == "__main__":
     main()
